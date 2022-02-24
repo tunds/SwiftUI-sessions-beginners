7bfc4ffa80db9868f4e51419df77af974134c6cb
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-02-24 17:30:04 +0000

    * Added source files

---
+++ Shapes in SwiftUI/ShapesInSwiftUI/ShapesInSwiftUI/ShapesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesInSwiftUIApp.swift
+//  ShapesInSwiftUI
+//
+//  Created by Tunde on 06/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShapesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
