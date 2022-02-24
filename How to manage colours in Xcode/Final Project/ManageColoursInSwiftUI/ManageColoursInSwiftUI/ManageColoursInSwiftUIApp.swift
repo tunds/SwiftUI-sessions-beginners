b5854fbf9e81540c4f3343d1a373b4023eb07b38
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-02-24 16:57:21 +0000

    * Added source files

---
+++ How to manage colours in Xcode/Final Project/ManageColoursInSwiftUI/ManageColoursInSwiftUI/ManageColoursInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManageColoursInSwiftUIApp.swift
+//  ManageColoursInSwiftUI
+//
+//  Created by Tunde on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ManageColoursInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
