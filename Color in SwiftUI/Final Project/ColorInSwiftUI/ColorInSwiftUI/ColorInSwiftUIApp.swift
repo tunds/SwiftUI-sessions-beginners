8abd43b1a8956947e5a830b8c3162a64aabac163
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-02-24 17:04:58 +0000

    * Added source files

---
+++ Color in SwiftUI/Final Project/ColorInSwiftUI/ColorInSwiftUI/ColorInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorInSwiftUIApp.swift
+//  ColorInSwiftUI
+//
+//  Created by Tunde on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
