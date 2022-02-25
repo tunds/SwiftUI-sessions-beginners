e22f855d5366157dbf36a2fb7ba700430b08c551
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-02-25 16:05:50 +0000

    * Added source files

---
+++ 13 Breaking down SwiftUI State & Data Flow/BreakingDownStateAndDataFlow/BreakingDownStateAndDataFlow/BreakingDownStateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreakingDownStateAndDataFlowApp.swift
+//  BreakingDownStateAndDataFlow
+//
+//  Created by Tunde on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct BreakingDownStateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
