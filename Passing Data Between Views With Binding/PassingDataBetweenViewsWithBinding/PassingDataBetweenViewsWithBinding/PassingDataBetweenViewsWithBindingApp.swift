5c702c4b81810bc1db80ea503a72872a279ece72
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-02-25 16:16:52 +0000

    * Added source files

---
+++ Passing Data Between Views With Binding/PassingDataBetweenViewsWithBinding/PassingDataBetweenViewsWithBinding/PassingDataBetweenViewsWithBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassingDataBetweenViewsWithBindingApp.swift
+//  PassingDataBetweenViewsWithBinding
+//
+//  Created by Tunde on 14/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PassingDataBetweenViewsWithBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
