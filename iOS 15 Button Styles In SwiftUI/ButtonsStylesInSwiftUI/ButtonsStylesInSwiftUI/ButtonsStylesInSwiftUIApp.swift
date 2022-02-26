a99b6843bebe5001192a2f3f91342ee61645892e
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-02-26 11:01:59 +0000

    * Added source files

---
+++ iOS 15 Button Styles In SwiftUI/ButtonsStylesInSwiftUI/ButtonsStylesInSwiftUI/ButtonsStylesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsStylesInSwiftUIApp.swift
+//  ButtonsStylesInSwiftUI
+//
+//  Created by Tunde Adegoroye on 04/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsStylesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
