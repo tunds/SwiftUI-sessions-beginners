b0a37fcfa385ad69ef2e6bf84c71fd5b6c423cdb
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-02-26 13:28:03 +0000

    * Added source files

---
+++ Background and overlay in SwiftUI/BackgroundAndOverlayInSwiftUI/BackgroundAndOverlayInSwiftUI/BackgroundAndOverlayInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundAndOverlayInSwiftUIApp.swift
+//  BackgroundAndOverlayInSwiftUI
+//
+//  Created by Tunde Adegoroye on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundAndOverlayInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
