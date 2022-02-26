dfc3ba9dd249ea015b558c8ad10b6b2252e4916b
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-02-26 11:24:12 +0000

    * Added source files

---
+++ TextField & SecureField in SwiftUI (SwiftUI, SwiftUI Tutorial, Login Form SwiftUI)/TextFieldAndSecureFieldInSwiftUI/TextFieldAndSecureFieldInSwiftUI/TextFieldAndSecureFieldInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldAndSecureFieldInSwiftUIApp.swift
+//  TextFieldAndSecureFieldInSwiftUI
+//
+//  Created by Tunde Adegoroye on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldAndSecureFieldInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
