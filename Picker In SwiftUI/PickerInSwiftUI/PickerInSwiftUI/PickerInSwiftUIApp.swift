7dab0184034c8e8df64aa626aa42b466c7bb9ef3
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-03-20 10:15:56 +0000

    Added source files

---
+++ Picker In SwiftUI/PickerInSwiftUI/PickerInSwiftUI/PickerInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerInSwiftUIApp.swift
+//  PickerInSwiftUI
+//
+//  Created by Tunde Adegoroye on 08/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct PickerInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AnimeView()
+        }
+    }
+}
