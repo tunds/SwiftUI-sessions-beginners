8bb4900b60be398fd66e9b6c7cf02f90d5947ee9
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-04-02 18:02:58 +0100

    Added source files

---
+++ Menu vs Context Menu/MenuVsContextMenuInSwiftUI/MenuVsContextMenuInSwiftUI/MenuVsContextMenuInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuVsContextMenuInSwiftUIApp.swift
+//  MenuVsContextMenuInSwiftUI
+//
+//  Created by Tunde Adegoroye on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MenuVsContextMenuInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
