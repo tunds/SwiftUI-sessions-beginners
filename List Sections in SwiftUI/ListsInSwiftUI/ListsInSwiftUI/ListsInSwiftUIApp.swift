1cbe1cffd4e4b65bf200ea1effe8b106ed3c6dd1
Tunde Adegoroye <Tunde.Adegoroye@degree53.com>
2022-04-02 17:52:28 +0100

    Added source files

---
+++ List Sections in SwiftUI/ListsInSwiftUI/ListsInSwiftUI/ListsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsInSwiftUIApp.swift
+//  ListsInSwiftUI
+//
+//  Created by Tunde Adegoroye on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
