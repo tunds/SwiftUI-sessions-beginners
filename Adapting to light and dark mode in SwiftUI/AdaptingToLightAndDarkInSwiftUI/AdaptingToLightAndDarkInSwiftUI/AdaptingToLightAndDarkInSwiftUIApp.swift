90e44a03f1968cc3c75d54befcabad4da6b6b631
Tunde Adegoroye <tundsdev@yahoo.co.uk>
2022-05-17 08:55:25 +0100

    * Added Source Files

---
+++ Adapting to light and dark mode in SwiftUI/AdaptingToLightAndDarkInSwiftUI/AdaptingToLightAndDarkInSwiftUI/AdaptingToLightAndDarkInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdaptingToLightAndDarkInSwiftUIApp.swift
+//  AdaptingToLightAndDarkInSwiftUI
+//
+//  Created by Tunde Adegoroye on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdaptingToLightAndDarkInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
